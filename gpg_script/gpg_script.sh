183d2900fd085f7f96f45c4264bdda55f6fd455a
Chalhotra <malhotrasoumil@gmail.com>
2024-03-19 02:06:06 +0530

    Update gpg_script.sh

--- gpg_script/gpg_script.sh
+++ gpg_script/gpg_script.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+source ./configure.sh
 banner() {
         echo "---------------------------------------------------------------------------------------"
     echo """                                
@@ -10,44 +11,118 @@ banner() {
    
     echo
     echo "---------------------------------------------------------------------------------------"
-
-
     
 }
 gpg_gen() {
     gpg --full-generate-key
+    echo
+    echo "------------------------------------------"
+    echo
 }
+
 add_to_git() {
-    echo "Select a key from the given list: "
+    echo "Select a key using its index from the given list: "
     echo 
-    gpg --list-secret-keys --keyid-format=long | grep "^sec" | awk '{print $2}'
-
+    counter=1
+    gpg --list-secret-keys --keyid-format=long | grep "^sec" | while read -r line; do
+        key_id=$(echo "$line" | awk '{print $2}' | awk -F "/" '{print $2}')
+        echo "$counter) $key_id"
+        ((counter++))
+        
+    done
     read n
-    gkey=$(gpg --list-secret-keys --keyid-format=long | grep "^sec" | awk '{print $2}'| awk -F '/' '{print $2}' | sed -n "${n}p")
+    integer_pattern='^[0-9]+$'
+    if [[ "$n" =~ $integer_pattern ]]; then
 
-    echo "Exporting selected key..."
-    gpg --armor --export "$gkey" > gpg_key.pub
-    git config --global user.signingkey "$gkey"
-    git config --global commit.gpgsign true
-    gpg --armour --export $gkey
-    echo "Successfully added this key to your Git configuration."
+        gkey=$(gpg --list-secret-keys --keyid-format=long | grep "^sec" | awk '{print $2}'| awk -F '/' '{print $2}' | sed -n "${n}p")
+
+        echo "Exporting selected key..."
+        gpg --armor --export "$gkey" > gpg_key.pub
+        git config --global user.signingkey "$gkey"
+        git config --global commit.gpgsign true
+        gpg --armour --export $gkey
+        echo "Successfully added this key to your Git configuration."
+        echo
+        echo "------------------------------------------"
+        echo
+    else
+        echo "Please choose a valid option"
+        true
+    fi
 }
-
 
 
 delete_key() {
-    echo "Enter the key ID you want to delete: "
+    echo "Enter the index of the key you want to delete: "
     echo
     
-    gpg --list-secret-keys --keyid-format=long
+    counter=1
+    gpg --list-secret-keys --keyid-format=long | grep "^sec" | while read -r line; do
+        key_id=$(echo "$line" | awk '{print $2}' | awk -F "/" '{print $2}')
+        echo "$counter) $key_id"
+        ((counter++))
+        
+    done
     read n
-    key_id=$(gpg --list-secret-keys --keyid-format=long | grep "^sec" | awk '{print $2}' |awk -F "/" '{print $2}'| sed -n "${n}p")
+    integer_pattern='^[0-9]+$'
+    if [[ "$n" =~ $integer_pattern ]]; then
 
+        key_count=$(gpg --list-secret-keys --keyid-format=long | grep "^sec" | wc -l)
 
+        if [ "$n" -le $key_count ];then
+            key_id=$(gpg --list-secret-keys --keyid-format=long | grep "^sec" | awk '{print $2}' |awk -F "/" '{print $2}'| sed -n "${n}p")
 
+            echo "Deleting the specified GPG key..."
+            gpg --delete-secret-keys "$key_id"
+            echo
+            echo "------------------------------------------"
+            echo
+        else
+            true
+        fi
+    else
+        echo "Please choose a valid option"
+        true
+    fi
     
-    echo "Deleting the specified GPG key..."
-    gpg --delete-secret-keys "$key_id"
+}
+view_key() {
+    counter=1
+    gpg --list-secret-keys --keyid-format=long | grep "^sec" | while read -r line; do
+        key_id=$(echo "$line" | awk '{print $2}' | awk -F "/" '{print $2}')
+        echo "$counter) $key_id"
+        ((counter++))
+
+    done
+
+    echo
+    echo "-----------------------------------------"
+    echo "-> Enter the index of the key whose PGP you would like to see"
+    echo
+    echo "-> 0 to Go back"
+
+    read n
+    
+    key_count=$(gpg --list-secret-keys --keyid-format=long | grep "^sec" | wc -l)
+    integer_pattern='^[0-9]+$'
+    if [[ "$n" =~ $integer_pattern ]]; then
+        if [ "$n" -eq 0 ]; then
+            true
+        
+        elif [ "$n" -le $key_count ]; then
+            key_id=$(gpg --list-secret-keys --keyid-format=long | grep "^sec" | awk '{print $2}' |awk -F "/" '{print $2}'| sed -n "${n}p")
+            
+            gpg --armour --export $key_id
+        else 
+            true
+        fi
+    else
+        echo "Please choose a valid option"
+        true
+    fi
+    echo
+    echo "------------------------------------------"
+    echo
     
 }
 
@@ -62,29 +137,34 @@ menu() {
     echo  
     echo -e "5.) Exit"
 
-    read -r choice
+    read choice
+    integer_pattern='^[0-9]+$'
+    if [[ "$choice" =~ $integer_pattern ]]; then
+        if [ "$choice" -eq 1 ]; then
+            gpg_gen
+            menu
+        elif [ "$choice" -eq 2 ]; then
+            add_to_git
+            menu
 
-    if [ "$choice" -eq 1 ]; then
-        gpg_gen
-        menu
-    elif [ "$choice" -eq 2 ]; then
-        add_to_git
-        menu
+        elif [ "$choice" -eq 4 ]; then
+            view_key
+            menu
 
-    elif [ "$choice" -eq 4 ]; then
-        gpg --list-secret-keys --keyid-format=long
-        menu
-    elif [ "$choice" -eq 3 ]; then
-        delete_key
-        menu
-    
 
-        
-    elif [ "$choice" -eq 5 ]; then
-        exit
-    else 
+        elif [ "$choice" -eq 3 ]; then
+            delete_key
+            menu
+            
+        elif [ "$choice" -eq 5 ]; then
+            exit
+        else 
+            echo "Please choose a valid option"
+            menu
+        fi
+    else
         echo "Please choose a valid option"
-        menu
+            menu
     fi
 }
 
